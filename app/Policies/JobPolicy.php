b230942985585752f8dbf1ad96000d4f1651b7ad
salma <salmafathy274@gmail.com>
2026-09-11 21:17:01 +0300

    Add admin bypass to jobpolicy

--- app/Policies/JobPolicy.php
+++ app/Policies/JobPolicy.php
@@ -9,7 +9,17 @@ use Illuminate\Auth\Access\Response;
 class JobPolicy
 {
    
+    public function before(User $user, string $ability): ?bool
+    {
+        if($user->isAdmin())
+            {
+                return true;
+            }
 
+    
+        return null;
+    
+    }
     /**
      * Determine whether the user can update the model.
      */
