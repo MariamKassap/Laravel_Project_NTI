f755080a25fc82b72b167b2f50833acb8cc9f973
salma <salmafathy274@gmail.com>
2026-09-16 06:48:35 +0300

    Added aliases for employee controllers

--- routes/web.php
+++ routes/web.php
@@ -2,9 +2,9 @@
 
 use App\Http\Controllers\ProfileController;
 
-use App\Http\Controllers\Employee\DashboardController;
-use App\Http\Controllers\Employee\JobController;
-use App\Http\Controllers\Employee\ApplicationController;
+use App\Http\Controllers\Employee\DashboardController as EmployeeDashboardController;
+use App\Http\Controllers\Employee\JobController as EmployeeJobController;
+use App\Http\Controllers\Employee\ApplicationController as EmployeeApplicationController;
 
 use App\Http\Controllers\Employer\DashboardController as EmployerDashboardController;
 use App\Http\Controllers\Employer\JobController as EmployerJobController;
@@ -20,23 +20,23 @@ Route::get('/', function () {
 Route::middleware(['auth', 'role:employee'])->group(function () {
 
     // Employee Dashboard
-    Route::get('/employee/dashboard', [DashboardController::class, 'index'])->name('employee.dashboard');
+    Route::get('/employee/dashboard', [EmployeeDashboardController::class, 'index'])->name('employee.dashboard');
 
     // Employee Applications
-    Route::get('/employee/applications', [ApplicationController::class, 'index'])->name('employee.applications.index');
+    Route::get('/employee/applications', [EmployeeApplicationController::class, 'index'])->name('employee.applications.index');
 
-    Route::get('/employee/applications/{application}', [ApplicationController::class, 'show'])->name('employee.applications.show');
+    Route::get('/employee/applications/{application}', [EmployeeApplicationController::class, 'show'])->name('employee.applications.show');
 
     // Apply for a job - show CV selection
-    Route::get('/employee/jobs/{job}/apply', [ApplicationController::class, 'create'])->name('employee.jobs.apply');
+    Route::get('/employee/jobs/{job}/apply', [EmployeeApplicationController::class, 'create'])->name('employee.jobs.apply');
 
     // Apply for a job - submit application
-    Route::post('/employee/jobs/{job}/apply', [ApplicationController::class, 'store'])->name('employee.jobs.store');
+    Route::post('/employee/jobs/{job}/apply', [EmployeeApplicationController::class, 'store'])->name('employee.jobs.store');
 
     // Employee Jobs
-    Route::get('/employee/jobs', [JobController::class, 'index'])->name('employee.jobs.index');
+    Route::get('/employee/jobs', [EmployeeJobController::class, 'index'])->name('employee.jobs.index');
 
-    Route::get('/employee/jobs/{job}', [JobController::class, 'show'])->name('employee.job.show');
+    Route::get('/employee/jobs/{job}', [EmployeeJobController::class, 'show'])->name('employee.job.show');
 });
 
 
